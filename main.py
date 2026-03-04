e6d988ca0b878cf0a34efcc8f633ed699241cbdb
One Look <onelookdec3@gmail.com>
2026-03-04 17:25:11 +0530

    updated telegram webhook URL to Railway deployment and changed port to 8080

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "api.application:app",
         host="0.0.0.0",
-        port=8000,
+        port=8080,
         reload=True,
         log_level="info"
     )
